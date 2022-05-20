5b89c0ac3d6e969e9744aedc8ec0ede5c990ab59
Mariana <mariana.amfr@gmail.com>
2022-05-20 08:57:15 +0100

    Fixed Redirect and added some styling

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -12,7 +12,7 @@ const credentials = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  redirect_uris: ['https://mareeanamaro.github.io/meet'],
+  redirect_uris: ['https://mareeanamaro.github.io/meet-v2'],
   javascript_origins: ['https://mareeanamaro.github.io/', 'http://localhost:300'],
 };
 const { client_id, client_secret, redirect_uris, calendar_id } = credentials;
